f2df076289ea9e684b34bcf9b40fdf7c9bdcc6f3
gigamicro <gammamicro@gmail.com>
2024-03-27 23:56:46 -0400

    remove $ from grineer charlist

--- javascripts/main.js
+++ javascripts/main.js
@@ -233,7 +233,7 @@ var grineer = new function(){
 	};
 
 	this.imgs = [];
-	this.chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'v', 'w', 'y', 'z', '?', '.', ',', '#', '@'/*logo*/, '$', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+	this.chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'v', 'w', 'y', 'z', '?', '.', ',', '#', '@'/*logo*/, '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 	for(var a = 0; a < this.chars.length; a++){ // gets images and puts them in imgs table
 		this.imgs[this.chars[a]] = new Image();
 		this.imgs[this.chars[a]].src = this.folder + this.pre + escapePunctuation(this.chars[a]) + this.ext;
@@ -367,7 +367,7 @@ var tenno = new function(){
 	this.consonants = [];
 
 	this.imgs = [];
-	this.chars = ['aye', 'ae', 'ow', 'aw', 'ee', 'i', 'e', 'a', 'u', 'oo' , 'o', 'th', 'dh', 'sh', 'zh', 'ch', 'kh', 'ng', 'p', 'b', 't', 'd', 's', 'z', 'j', 'k', 'g', 'f', 'v', 'm', 'n', 'h', 'r', 'l', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', ',', '-'];
+	this.chars = ['aye', 'ae', 'ow', 'aw', 'ee', 'i', 'e', 'a', 'u', 'oo', 'o', 'th', 'dh', 'sh', 'zh', 'ch', 'kh', 'ng', 'p', 'b', 't', 'd', 's', 'z', 'j', 'k', 'g', 'f', 'v', 'm', 'n', 'h', 'r', 'l', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', ',', '-'];
 	for(var ch of this.chars){
 		this.imgs[ch] = new Image();
 		this.imgs[ch].src = this.folder + this.pre + escapePunctuation(ch) + this.ext;
